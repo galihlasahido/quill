d13634d4b2be03e30401cc6b018312f0d096c668
Galih Lasahido <galih.lasahido@gmail.com>
2025-12-11 15:02:50 +0700

    Expand font size whitelist for improved text styling options

--- packages/quill/src/formats/size.ts
+++ packages/quill/src/formats/size.ts
@@ -6,7 +6,7 @@ const SizeClass = new ClassAttributor('size', 'ql-size', {
 });
 const SizeStyle = new StyleAttributor('size', 'font-size', {
   scope: Scope.INLINE,
-  whitelist: ['10px', '18px', '32px'],
+  whitelist: ['8pt', '9pt', '10pt', '11pt', '12pt', '14pt', '16pt', '18pt', '20pt', '22pt', '24pt', '26pt', '28pt', '36pt', '48pt', '72pt'],
 });
 
 export { SizeClass, SizeStyle };
