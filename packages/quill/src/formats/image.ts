f78d24c2e8a38978a78d29381ad3754b859f5e04
Galih Lasahido <galih.lasahido@gmail.com>
2025-12-12 14:27:25 +0700

    Enhance image handling in Quill editor by adding new attributes and inline style support

--- packages/quill/src/formats/image.ts
+++ packages/quill/src/formats/image.ts
@@ -1,22 +1,36 @@
 import { EmbedBlot } from 'parchment';
 import { sanitize } from './link.js';
 
-const ATTRIBUTES = ['alt', 'height', 'width'];
+const ATTRIBUTES = [
+  'alt',
+  'height',
+  'width',
+  'data-border-width',
+  'data-border-color',
+  'data-border-style',
+  'data-margin-top',
+  'data-margin-right',
+  'data-margin-bottom',
+  'data-margin-left',
+  'data-float'
+];
 
 class Image extends EmbedBlot {
   static blotName = 'image';
   static tagName = 'IMG';
 
   static create(value: string) {
-    const node = super.create(value) as Element;
+    const node = super.create(value) as HTMLImageElement;
     if (typeof value === 'string') {
       node.setAttribute('src', this.sanitize(value));
     }
+    // Default cursor for interactivity
+    node.style.cursor = 'pointer';
     return node;
   }
 
   static formats(domNode: Element) {
-    return ATTRIBUTES.reduce(
+    const formats = ATTRIBUTES.reduce(
       (formats: Record<string, string | null>, attribute) => {
         if (domNode.hasAttribute(attribute)) {
           formats[attribute] = domNode.getAttribute(attribute);
@@ -25,10 +39,19 @@ class Image extends EmbedBlot {
       },
       {},
     );
+
+    // Also capture inline styles
+    const element = domNode as HTMLElement;
+    if (element.style.border) formats['style-border'] = element.style.border;
+    if (element.style.margin) formats['style-margin'] = element.style.margin;
+    if (element.style.float) formats['style-float'] = element.style.float;
+    if (element.style.display) formats['style-display'] = element.style.display;
+
+    return formats;
   }
 
   static match(url: string) {
-    return /\.(jpe?g|gif|png)$/.test(url) || /^data:image\/.+;base64/.test(url);
+    return /\.(jpe?g|gif|png|svg|webp)$/.test(url) || /^data:image\/.+;base64/.test(url);
   }
 
   static sanitize(url: string) {
@@ -47,6 +70,14 @@ class Image extends EmbedBlot {
         this.domNode.setAttribute(name, value);
       } else {
         this.domNode.removeAttribute(name);
+      }
+    } else if (name.startsWith('style-')) {
+      // Handle inline style formats
+      const styleName = name.replace('style-', '');
+      if (value) {
+        this.domNode.style[styleName as any] = value;
+      } else {
+        this.domNode.style[styleName as any] = '';
       }
     } else {
       super.format(name, value);
